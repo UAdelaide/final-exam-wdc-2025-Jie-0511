ed68d8182f2d4201a1b0d9358d0b5e166f39c383
Jie <lijie20221606@163.com>
2025-06-20 10:03:52 +0000

    [automatic commit] 2025-06-20 10:03:52 UTC 49.178.121.178

--- part2/app.js
+++ part2/app.js
@@ -15,7 +15,8 @@ const userRoutes = require('./routes/userRoutes');
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
 
-//session
+// session
+const 
 
 // Export the app instead of listening here
 module.exports = app;

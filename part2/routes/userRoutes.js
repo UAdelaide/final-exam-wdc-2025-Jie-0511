bca90cc04c4aac0faa016ccf2ce9849765a725ec
Jie <lijie20221606@163.com>
2025-06-20 09:55:31 +0000

    [automatic commit] 2025-06-20 09:55:31 UTC 49.178.121.178

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -52,6 +52,8 @@ router.post('/login', async (req, res) => {
     const user = rows[0];
     req.session.user = user;
 
+    const redirectu
+
     res.json({ message: 'Login successful', user: rows[0] });
   } catch (error) {
     res.status(500).json({ error: 'Login failed' });

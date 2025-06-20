ac72bdd99d1cc79130ca1c3c5d19ff6158650835
Jie <lijie20221606@163.com>
2025-06-20 08:11:37 +0000

    [automatic commit] 2025-06-20 08:11:37 UTC 49.178.121.178

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -4,18 +4,7 @@ const router = express.Router();
 const db = require('../db');
 
 // 6
-router.get('/dogs', async (req, res) => {
-    try {
-        const [rows] = await db.query(`
-      SELECT Dogs.name AS dog_name, Dogs.size, Users.username AS owner_username
-      FROM Dogs
-      JOIN Users ON Dogs.owner_id = Users.user_id
-    `);
-        res.json(rows);
-    } catch (err) {
-        res.status(500).json({ error: 'Failed to fetch dogs' });
-    }
-});
+rou
 
 // 7
 router.get('/walkrequests/open', async (req, res) => {

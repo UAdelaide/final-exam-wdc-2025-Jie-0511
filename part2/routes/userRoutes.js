67be4983b6b1de3b7070318c224020111d8bb7ff
Jie <lijie20221606@163.com>
2025-06-20 09:54:50 +0000

    [automatic commit] 2025-06-20 09:54:50 UTC 49.178.121.178

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -49,7 +49,8 @@ router.post('/login', async (req, res) => {
       return res.status(401).json({ error: 'Invalid credentials' });
     }
 
-    const user = rows[0]
+    const user = rows[0];
+    req.session.user
 
     res.json({ message: 'Login successful', user: rows[0] });
   } catch (error) {

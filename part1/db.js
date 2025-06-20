d0592bc02b84588994b96de43b3f106281bf09aa
Jie <lijie20221606@163.com>
2025-06-20 10:31:42 +0000

    [automatic commit] 2025-06-20 10:31:42 UTC 49.178.121.178

--- part1/db.js
+++ part1/db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
     // host: 'localhost',
-    host:"/var/run/mysqld/mysqld.sock",
+    // host:"/var/run/mysqld/mysqld.sock",
     user: 'root',
     password: '',
     database: 'DogWalkService'

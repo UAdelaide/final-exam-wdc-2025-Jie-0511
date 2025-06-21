d9e1b0edc74208986b7e1aba957d041fa1e6434f
Jie <lijie20221606@163.com>
2025-06-21 02:31:54 +0000

    [automatic commit] 2025-06-21 02:31:54 UTC 129.127.202.8

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -41,6 +41,7 @@ router.post('/login', async (req, res) => {
   const { email, password } = req.body;
 
   try {
+    //
     const [rows] = await db.query(`
       SELECT user_id, username, role FROM Users
       WHERE email = ? AND password_hash = ?

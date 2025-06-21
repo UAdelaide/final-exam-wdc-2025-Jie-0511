1d1ccd7145fc982ba7c034c838b0478dacceafe6
Jie <lijie20221606@163.com>
2025-06-21 03:04:24 +0000

    [automatic commit] 2025-06-21 03:04:24 UTC 129.127.202.8

--- part2/routes/walkRoutes.js
+++ part2/routes/walkRoutes.js
@@ -59,7 +59,7 @@ router.post('/:id/apply', async (req, res) => {
   }
 });
 
-
+// Get all dogs owned by the 
 router.get('/owner/dogs', async (req, res) => {
   if (!req.session.user || req.session.user.role !== 'owner') {
     return res.status(401).json({ error: 'Unauthorized' });
